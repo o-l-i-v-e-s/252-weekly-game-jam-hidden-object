7517ee8748e8edd9c1f68b59a2c5c0bef5f1f801
Marko Jovanovic <artist.olives@gmail.com>
2022-05-10 07:27:07 -0700

    Change how app quits depending on environment

--- Assets/Scripts/Menus.cs
+++ Assets/Scripts/Menus.cs
@@ -33,7 +33,16 @@ public class Menus : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit();
+        #if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+                Debug.Log(this.name + " : " + this.GetType() + " : " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        #endif
+        #if (UNITY_EDITOR)
+                UnityEditor.EditorApplication.isPlaying = false;
+        #elif (UNITY_STANDALONE)
+                    Application.Quit();
+        #elif (UNITY_WEBGL)
+                SceneManager.LoadScene("StartMenu");
+        #endif
     }
 
     public void PauseGame()
